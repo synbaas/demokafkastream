6e249275930d6db3c67df34b709a5c57005f5d99
synbaas <synbaas@gmail.com>
2019-05-07 10:50:45 +0530

    Push all code

---
+++ src/test/java/com/syntel/demokafkastream/DemokafkastreamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syntel.demokafkastream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemokafkastreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
